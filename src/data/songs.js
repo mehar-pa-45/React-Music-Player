61e0cabdb04eed9a37ec8c7683ec745b2c1a6e2e
mehar-pa-45 <mehar.pavan77@gmail.com>
2026-03-20 13:30:22 +0530

    Create songs.js

---
+++ src/data/songs.js
@@ -0,0 +1,18 @@
+const songs = [
+  {
+    id: 1,
+    title: "Kesariya",
+    artist: "Arijit Singh",
+    cover: "/covers/cover1.jpg",
+    url: "/songs/song1.mp3"
+  },
+  {
+    id: 2,
+    title: "Raataan Lambiyan",
+    artist: "Jubin Nautiyal",
+    cover: "/covers/cover2.jpg",
+    url: "/songs/song2.mp3"
+  }
+];
+
+export default songs;
